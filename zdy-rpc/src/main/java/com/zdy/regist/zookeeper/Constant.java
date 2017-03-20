f853535d011e2a4974653a272bdf97fb80fdab7d
zdylalala <zdylalala@users.noreply.github.com>
2017-03-20 09:01:53 +0800

    项目初始化
    项目初始化

---
+++ zdy-rpc/src/main/java/com/zdy/regist/zookeeper/Constant.java
@@ -0,0 +1,15 @@
+package com.zdy.regist.zookeeper;
+
+/**
+ * 常量
+ *
+ * @author zdy
+ * @since 1.0.0
+ */
+public interface Constant {
+
+    int ZK_SESSION_TIMEOUT = 5000;
+    int ZK_CONNECTION_TIMEOUT = 1000;
+
+    String ZK_REGISTRY_PATH = "/registry";
+}
